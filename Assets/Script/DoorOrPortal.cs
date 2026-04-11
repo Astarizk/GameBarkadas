e9e3f13e988bfff6375932c33ba2927874a4f822
NikkiCutie4405 <ikkinikki47@gmail.com>
2026-04-11 17:16:47 +0800

    Door

--- Assets/Script/DoorOrPortal.cs
+++ Assets/Script/DoorOrPortal.cs
@@ -1,23 +1,42 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DoorOrPortal : MonoBehaviour
 {
     public Transform destination;
     private GameObject player;
 
+    private bool isPlayerInside = false;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (isPlayerInside && Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            if (Vector2.Distance(player.transform.position, transform.position) > 0.3f)
+            {
+                player.transform.position = destination.position;
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            if (Vector2.Distance(player.transform.position, transform.position) > 0.3f)
-            {
-                player.transform.position = destination.position;
-            }
+            isPlayerInside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInside = false;
         }
     }
 }

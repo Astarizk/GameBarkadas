1baea5284b199d4f38b1fe7e467591fb6f4d9a67
NikkiCutie4405 <ikkinikki47@gmail.com>
2026-05-17 14:16:50 +0800

    Floor1and2

--- Assets/Script/DoorOpen.cs
+++ Assets/Script/DoorOpen.cs
@@ -10,17 +10,21 @@ public class DoorOpen : MonoBehaviour
     public bool requiresKey2;
     public GameObject Door;
     private bool playerNear = false;
-
+    public bool Needs2Key;
     void Update()
     {
         if (playerNear && Keyboard.current.eKey.wasPressedThisFrame)
         {
             // Check correct key
-            if (requiresKey1 && Manager.gameItem1Picked)
+            if (requiresKey1 && Manager.gameItem1Picked && Needs2Key == false)
             {
                 OpenDoor();
             }
-            else if (requiresKey2 && Manager.gameItem2Picked)
+            else if (requiresKey2 && Manager.gameItem2Picked && Needs2Key == false)
+            {
+                OpenDoor();
+            }
+            else if (Manager.gameItem1Picked && Manager.gameItem2Picked && Needs2Key == true)
             {
                 OpenDoor();
             }
